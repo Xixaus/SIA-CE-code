2cfbd6eab8e6b340bff6d5cbf4bea99db9641d10
Xixaus <104562795+Xixaus@users.noreply.github.com>
2025-08-18 14:21:59 +0200

    Delete SIA-CE/SIA_API directory

--- SIA-CE/SIA_API/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Core components for SIA.
-"""
-
-from .command_sender import CommandSender
-
-__all__ = ['CommandSender']
--- SIA-CE/SIA_API/devices/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Device controllers for SIA.
-"""
-
-from .syringe_controller import SyringeController
-from .valve_selector import ValveSelector
-
-__all__ = ['SyringeController', 'ValveSelector']
